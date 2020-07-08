893958a62fa78f2b4ccb856b17a86c1455e3ec89
Jeoffrey <jeoffrey_yang@outlook.com>
2020-07-08 23:48:43 +0800

    create new file Twosum

--- src/main/java/cn/test/demo/Twosum.java
+++ src/main/java/cn/test/demo/Twosum.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Twosum {
     public static void main(String args[]){
         int target = 9;
-        int[] nums = {2, 7, 11, 15};
+        int[] nums = {2, 7, 11, 15,20,10};
         Solution s = new Solution();
         int []a = s.sum(nums,target);
         System.out.println(Arrays.toString(a));
