8cbd5f7fd8338d875f2fb02bf9fa93d9d0ab11f8
Jeoffrey Yang <gyang@sequoiacap.com>
2020-07-16 07:10:03 +0800

    add recursive with memorized

--- src/main/java/cn/test/demo/Climbstairs.java
+++ src/main/java/cn/test/demo/Climbstairs.java
@@ -4,7 +4,7 @@ import javax.print.attribute.standard.PrinterResolution;
 
 public class Climbstairs {
     public static void main(String[] args) {
-        int target = 3;
+        int target = 5;
         Solution s = new Solution();
         int steps = s.climbStairs(target);
         System.out.println(steps);
@@ -13,15 +13,20 @@ public class Climbstairs {
 
 class Solution{
     public int climbStairs(int n){
-        return climb_stairs(0,n);
+        int mem[] = new int[n+1];
+        return climb_stairs(0,n,mem);
     }
-    public int climb_stairs(int i,int n){
+    public int climb_stairs(int i,int n,int mem[]){
         if(i > n){
             return 0;
         }
         if(i == n){
             return 1;
         }
-        return climb_stairs(i+1,n) + climb_stairs(1+2,n);
+        if(mem[i]>0){
+           return mem[i] ;
+        }
+        mem[i] =  climb_stairs(i+1,n,mem) + climb_stairs(1+2,n,mem);
+        return mem[i];
     }
 }
